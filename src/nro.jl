a06cf2fc5744245ae911ba51e271f516d958f90c
Ujjwal Sarswat <ujjwalsarswat1@gmail.com>
2023-03-26 17:00:48 +0530

    start with 2 class classification sampling

--- src/nro.jl
+++ src/nro.jl
@@ -1,6 +1,17 @@
 module nro
 
 import CSV
-import DataFrames
+using DataFrames, MLUtils
+
+# assuming 2-class classification
+
+function naive_random_oversampler(X, y::AbstractVector)
+    classes = unique(y)
+    classcount = Dict(c => count(y .== c) for c in classes)
+    minorityclass = argmin(classcount)
+    surplus = maximum(classcount) - minimum(classcount)
+    minorityidx = findall(y .== minorityclass)
+    
+end
 
 end # module nro
