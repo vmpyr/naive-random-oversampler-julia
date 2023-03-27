1d7b5a1850345c1cb4cac5ada6f28e72175bafd4
Ujjwal Sarswat <ujjwalsarswat1@gmail.com>
2023-03-27 14:00:22 +0530

    complete demo script and comment annotate everything

--- demonstration/demo_script.jl
+++ demonstration/demo_script.jl
@@ -1,9 +1,24 @@
+# import the required packages
 import CSV
-using DataFrames, CategoricalArrays, DelimitedFiles, Nro
-X = CSV.read("/home/vmpyr/gsoc-julia/proposal/naive-random-oversampler-julia/data/pima-indians-diabetes-X.csv", DataFrame; header=["x1","x2","x3","x4","x5","x6","x7","x8"])
-y = CategoricalVector(vec(readdlm("/home/vmpyr/gsoc-julia/proposal/naive-random-oversampler-julia/data/pima-indians-diabetes-y.csv", Int)))
+using Nro, DataFrames, CategoricalArrays, DelimitedFiles
 
-Xnew, ynew = naive_random_oversampler(X, y)
+# load the data as defined in the task guidelines
+# load X as a DataFrame
+X = CSV.read("./data/pima-indians-diabetes-X.csv", DataFrame; header=["x1","x2","x3","x4","x5","x6","x7","x8"])
+# load y as a CategoricalVector
+y = CategoricalVector(vec(readdlm("./data/pima-indians-diabetes-y.csv", Int)))
 
-println(Xnew)
-println(ynew)
+# call the oversampler
+Xover, yover = naive_random_oversampler(X, y)
+
+# print the results
+show(Xover)
+println()
+show(IOContext(stdout, :limit => true), yover)
+println()
+
+# print the counts of the classes
+println("initial count of class 0: ", count(y .== 0))
+println("initial count of class 1: ", count(y .== 1))
+println("count of class 0 after oversampling: ", count(yover .== 0))
+println("count of class 1 after oversampling: ", count(yover .== 1))
--- src/Nro.jl
+++ src/Nro.jl
@@ -2,25 +2,40 @@ module Nro
 
 export naive_random_oversampler
 
+# using MLUtils for getobs
 using MLUtils
 
 # assuming 2-class classification
-
 function naive_random_oversampler(X, y::AbstractVector)
+    # retrieve the classes
     classes = unique(y)
+
+    # count the number of samples in each class
     classcount = Dict(c => count(y .== c) for c in classes)
+
+    # find the minority and majority classes
     minorityclass = argmin(classcount)
     majorityclass = argmax(classcount)
-    surplus = classcount[majorityclass] - classcount[minorityclass]
-    minorityidxs = findall(y .== minorityclass)
 
+    # calculate the number of samples (surplus) to be added
+    surplus = classcount[majorityclass] - classcount[minorityclass]
+
+    # randomly select surplus number of samples from the minority class
+    minorityidxs = findall(y .== minorityclass)
     rndidxs = rand(minorityidxs, surplus)
+
+    # create a copy of the dataset
+    Xover = copy(X)
+    yover = copy(y)
+
+    # add the samples to the copied dataset
     for idx in rndidxs
-        push!(X, getobs(X, idx))
-        push!(y, y[idx])
+        push!(Xover, getobs(X, idx))
+        push!(yover, y[idx])
     end
 
-    return X, y
+    # return the new dataset
+    return Xover, yover
 end
 
 end # module Nro
